6ebfcf1e92c9f093fca485a7b934b57cf9e0aa1c
Alex Jf <anunciosads10@gmail.com>
2026-01-05 20:13:46 +0000

    mira la imagen persite el problema:revisa la credeciales :// Import the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
   "projectId": "partnerverse-b43de",
-  "appId": "1:968907418402:web:140af3096ca5ed2292537b",
-  "apiKey": "AIzaSyAFZhU_6wMPguwfQiOaydBc9lzCguMNsV4",
+  "appId": "1:403856142238:web:f8756c01f1d6ab4e6ef08f",
+  "apiKey": "AIzaSyBIZzZCaV2AI0TiDtNF5ypj7HmBOwiJVZU",
   "authDomain": "partnerverse-b43de.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "968907418402"
+  "storageBucket": "partnerverse-b43de.appspot.com",
+  "messagingSenderId": "403856142238",
+  "measurementId": "G-X1NFFLEMDL"
 };

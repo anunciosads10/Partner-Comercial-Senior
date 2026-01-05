ab751be989e50c0ff07734567bcbd3738181a889
Alex Jf <anunciosads10@gmail.com>
2026-01-05 19:36:10 +0000

    error:## Error Type Console Error
    ## Error Message
    A tree hydrated but

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,16 +2,7 @@
 
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Search } from "lucide-react";
+import { Search, LogOut } from "lucide-react";
 import { Input } from "./ui/input";
 import { useAuth } from "@/firebase";
 import { signOut } from "firebase/auth";
@@ -37,24 +28,10 @@ export function Header() {
         />
       </div>
       <div className="flex flex-1 items-center justify-end gap-4">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon" className="rounded-full">
-              <Avatar>
-                <AvatarImage src="https://i.pravatar.cc/150?u=superadmin" alt="Super Admin" />
-                <AvatarFallback>SA</AvatarFallback>
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Super Admin</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onSelect={handleLogout}>Logout</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Button variant="ghost" onClick={handleLogout}>
+          <LogOut className="mr-2 h-4 w-4" />
+          Logout
+        </Button>
       </div>
     </header>
   );

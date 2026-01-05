500d161c6ea2440c3a75e81436010d192751baa0
Alex Jf <anunciosads10@gmail.com>
2026-01-05 20:07:43 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-5117747892-27ba3",
-  "appId": "1:714200046748:web:ffd40da95a2b5a71656159",
-  "apiKey": "AIzaSyADPWYnkAZWofoWoak0tMMxIUhBxSwfuTQ",
-  "authDomain": "studio-5117747892-27ba3.firebaseapp.com",
+  "projectId": "partnerverse-b43de",
+  "appId": "1:968907418402:web:140af3096ca5ed2292537b",
+  "apiKey": "AIzaSyAFZhU_6wMPguwfQiOaydBc9lzCguMNsV4",
+  "authDomain": "partnerverse-b43de.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "714200046748"
+  "messagingSenderId": "968907418402"
 };

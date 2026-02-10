abd4e0a9a44329f2495022ed98984d1ede95c6cd
Alex Jf <anunciosads10@gmail.com>
2026-02-10 00:04:25 +0000

    Corriguiendo barra later movil

--- src/app/(dashboard)/dashboard/page.jsx
+++
@@ -1,5 +0,0 @@
-/**
- * ARCHIVO DESACTIVADO ESTRUCTURALMENTE
- * Para resolver el error de Next.js 15 "Parallel Pages", este archivo NO debe exportar nada.
- * La página activa del dashboard se encuentra en: /src/app/dashboard/page.jsx
- */
